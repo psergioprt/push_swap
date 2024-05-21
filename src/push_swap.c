6b6240f7d7095efe3222fd0ccb61174f10ca3892
psergioprt <paulo.santossilva@gmail.com>
2024-05-21 14:15:51 +0100

    update

--- src/push_swap.c
+++ src/push_swap.c
@@ -3,15 +3,15 @@
 int	main(int argc, char *argv[])
 {
 	t_stack_node	*a;
-	t_stack_node	*b;
+	//t_stack_node	*b;
 
 	a = NULL;
-	b = NULL;
+	//b = NULL;
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return (1);
 	else if (argc == 2)
 		argv = ft_split(argv[1], ' ');
-	init_left_stack(&left_stack, argv + 1);
+	init_left_stack(&a, argv + 1);
 	print_stack(a);
 	free_stack(&a);
 	return (0);
